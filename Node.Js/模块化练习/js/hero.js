021a13d64fe9d8e2326cbde0539b89c966fe9eb3
cxyzsbd <985988042@qq.com>
2022-11-05 23:04:44 +0800

    提交

---
+++ Node.Js/模块化练习/js/hero.js
@@ -0,0 +1,10 @@
+
+export default class Hero {
+  constructor(name, ico, img, skills, skins) {
+    this.name = name;
+    this.ico = ico; //英雄小图
+    this.img = img; //英雄大图
+    this.skills = skills; //英雄技能
+    this.skins = skins; //英雄皮肤
+  }
+}
